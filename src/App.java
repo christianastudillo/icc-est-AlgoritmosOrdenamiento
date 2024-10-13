0bf33725602552bd920bda38549bf7fb87713618
Christian Astudillo <chris.astu134@gmail.com>
2024-10-12 22:18:38 -0500

    Pruebas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,83 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        MetodoOrdenamiento mo = new MetodoOrdenamiento();
+        int[] arreglo = null;
+        while (true) {
+            System.out.println("\nMenú Principal:");
+            System.out.println("1. Ingresar un arreglo");
+            System.out.println("2. Ordenar arreglo");
+            System.out.println("0. Salir");
+            int opcion = leerOpcion(scanner, 0, 2);  
+            switch (opcion) {
+                case 1:
+                    arreglo = mo.crearArreglo(scanner);
+                    break;
+                case 2:
+                    if (arreglo != null) {
+                        System.out.println("Elija un método de ordenamiento:");
+                        System.out.println("1. Selección");
+                        System.out.println("2. Inserción");
+                        System.out.println("3. Burbuja");
+                        System.out.println("4. Burbuja mejorada");
+                        int metodo = leerOpcion(scanner, 1, 4);  
+
+                        System.out.println("1. Ascendente");
+                        System.out.println("2. Descendente");
+                        boolean ascendente = leerOpcion(scanner, 1, 2) == 1;
+
+                        System.out.println("¿Desea ver los pasos?");
+                        System.out.println("1. Sí");
+                        System.out.println("2. No");
+                        boolean verPasos = leerOpcion(scanner, 1, 2) == 1;
+
+                        switch (metodo) {
+                            case 1:
+                                arreglo = mo.sortBySeleccion(arreglo, ascendente, verPasos);
+                                break;
+                            case 2:
+                                arreglo = mo.sortInserccion(arreglo, verPasos);
+                                break;
+                            case 3:
+                                arreglo = mo.sortByBubble(arreglo, ascendente, verPasos);
+                                break;
+                            case 4:
+                                arreglo = mo.sortBubbleAva(arreglo, verPasos);
+                                break;
+                            default:
+                                System.out.println("Método inválido.");
+                        }
+
+                        mo.printArreglo(arreglo);
+                    } else {
+                        System.out.println("Por favor, ingrese un arreglo primero.");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Saliendo...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        }
+    }
+
+    private static int leerOpcion(Scanner scanner, int min, int max) {
+        int opcion = -1;
+        while (opcion < min || opcion > max) {
+            System.out.print("Ingrese una opción (" + min + "-" + max + "): ");
+            if (scanner.hasNextInt()) {
+                opcion = scanner.nextInt();
+                if (opcion < min || opcion > max) {
+                    System.out.println("Opción fuera de rango. Intente de nuevo.");
+                }
+            } else {
+                System.out.println("Entrada no válida. Debe ingresar un número.");
+                scanner.next(); 
+            }
+        }
+        return opcion;
     }
 }
---
+++ src/MetodoOrdenamiento.java
@@ -0,0 +1,137 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MetodoOrdenamiento {
+
+    // selección
+    public int[] sortBySeleccion(int[] arreglo, boolean ascendente, boolean verPasos) {
+        int tamanio = arreglo.length;
+
+        for (int i = 0; i < tamanio; i++) {
+            int indice = i;
+            for (int j = i + 1; j < tamanio; j++) {
+                if ((ascendente && arreglo[j] < arreglo[indice]) || (!ascendente && arreglo[j] > arreglo[indice])) {
+                    indice = j;
+                }
+            }
+            // Intercambio
+            int aux = arreglo[indice];
+            arreglo[indice] = arreglo[i];
+            arreglo[i] = aux;
+
+            if (verPasos) {
+                System.out.println("Iteración " + (i + 1) + ": Intercambio de " + arreglo[i] + " con " + aux);
+                System.out.println("Arreglo: " + Arrays.toString(arreglo));
+            }
+        }
+        return arreglo;
+    }
+
+    // inserción
+    public int[] sortInserccion(int[] arreglo, boolean verPasos) {
+        int tamanio = arreglo.length;
+
+        for (int i = 1; i < tamanio; i++) {
+            int aux = arreglo[i];
+            int j = i - 1;
+            while (j >= 0 && arreglo[j] > aux) {
+                arreglo[j + 1] = arreglo[j];
+                j--;
+            }
+            arreglo[j + 1] = aux;
+
+            if (verPasos) {
+                System.out.println("Iteración " + i + ": Insertado " + aux + " en la posición " + (j + 1));
+                System.out.println("Arreglo: " + Arrays.toString(arreglo));
+            }
+        }
+        return arreglo;
+    }
+
+    // burbuja
+    public int[] sortByBubble(int[] arreglo, boolean ascendente, boolean verPasos) {
+        int n = arreglo.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if ((ascendente && arreglo[j] > arreglo[j + 1]) || (!ascendente && arreglo[j] < arreglo[j + 1])) {
+                    int aux = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+                }
+            }
+
+            if (verPasos) {
+                System.out.println("Iteración " + i + ": " + Arrays.toString(arreglo));
+            }
+        }
+        return arreglo;
+    }
+
+    // burbuja mejorada
+    public int[] sortBubbleAva(int[] arreglo, boolean verPasos) {
+        int n = arreglo.length;
+        boolean intercambio = false;
+
+        for (int i = 0; i < n; i++) {
+            intercambio = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arreglo[j] > arreglo[j + 1]) {
+                    int aux = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+                    intercambio = true;
+                }
+            }
+            if (!intercambio) {
+                break;
+            }
+
+            if (verPasos) {
+                System.out.println("Iteración " + i + ": " + Arrays.toString(arreglo));
+            }
+        }
+        return arreglo;
+    }
+
+    public int[] crearArreglo(Scanner scanner) {
+        int nElementos = -1;
+        while (nElementos <= 0) {
+            System.out.print("Introduce el tamaño del arreglo: ");
+            if (scanner.hasNextInt()) {
+                nElementos = scanner.nextInt();
+                if (nElementos <= 0) {
+                    System.out.println("El número de elementos debe ser mayor que cero");
+                }
+            } else {
+                System.out.println("Error, ingresar un número entero");
+                scanner.next(); 
+            }
+        }
+
+        int[] arreglo = new int[nElementos];
+
+        for (int i = 0; i < nElementos; i++) {
+            arreglo[i] = leerEntero(scanner, i + 1);
+        }
+        return arreglo;
+    }
+
+    private int leerEntero(Scanner scanner, int indice) {
+        int numero = Integer.MIN_VALUE;
+        while (numero == Integer.MIN_VALUE) {
+            System.out.print("Introduce el valor para el elemento " + indice + ": ");
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+            } else {
+                System.out.println("Error, ingresar un número entero");
+                scanner.next(); 
+            }
+        }
+        return numero;
+    }
+
+    public void printArreglo(int[] arreglo) {
+        System.out.println("Arreglo ordenado: " + Arrays.toString(arreglo));
+    }
+}
